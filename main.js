a47746e06bf3fc9f0d0388d63e853088c9790781
JaydenLee1116 <dltl5@yonsei.ac.kr>
2022-08-04 21:04:51 +0900

    초록색 되기 전 클릭 방지

--- main.js
+++ main.js
@@ -4,18 +4,22 @@ const $result = document.querySelector('#result');
 let startTime;
 let endTime;
 let records = [];
+let timeoutId;
 $screen.addEventListener('click', (event) => {
   if (event.target.classList.contains('waiting')) {
     $screen.classList.replace('waiting', 'ready');
     $screen.textContent = '초록색이 되면 클릭해주세요';
     // 파란 화면에서 클릭하고 빨강에서 초록되는 함수가 실행되어야 한다.
-    setTimeout(() => {
+    timeoutId = setTimeout(() => {
       startTime = new Date();
       $screen.classList.replace('ready', 'now');
       $screen.textContent = '클릭하세요!';
       // 첫 시간은 여기서부터 재야한다.
     }, Math.floor((Math.random() + 2) * 1000));
   } else if (event.target.classList.contains('ready')) {
+    clearTimeout(timeoutId);
+    $screen.classList.replace('ready', 'waiting');
+    $screen.innerHTML = '너무 성급하시군요! <br/>다시 시작하세요';
   } else if (event.target.classList.contains('now')) {
     // 끝 시간은 여기서부터 재야한다.
     endTime = new Date();
